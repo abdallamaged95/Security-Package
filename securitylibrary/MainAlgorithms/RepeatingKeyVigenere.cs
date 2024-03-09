4f37bfc4befa6985c57adb344e036ca7423749a1
abdalla_maged <100102723+abdallamaged95@users.noreply.github.com>
2024-03-10 00:58:12 +0200

    RepVignere

--- securitylibrary/MainAlgorithms/RepeatingKeyVigenere.cs
+++ securitylibrary/MainAlgorithms/RepeatingKeyVigenere.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Remoting.Messaging;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,17 +11,69 @@ namespace SecurityLibrary
     {
         public string Analyse(string plainText, string cipherText)
         {
-            throw new NotImplementedException();
+            plainText = plainText.ToLower();
+            cipherText = cipherText.ToLower();
+            string key = "";
+            for (int i = 0; i < plainText.Length; i++)
+            {
+                int x = (int)(plainText[i] - 'a'), z = (int)(cipherText[i] - 'a');
+                char y = (char)(((((z - x) % 26) + 26) % 26) + 'a');
+                key += y;
+            }
+            key = RemoveRepeated(key);
+            return key;
         }
 
         public string Decrypt(string cipherText, string key)
         {
-            throw new NotImplementedException();
+            cipherText = cipherText.ToLower();
+            if (key.Length != cipherText.Length)
+                key = GetKeyStream(key, cipherText);
+            string plainText = "";
+            for (int i = 0 ; i < key.Length; i++)
+            {
+                int x = (int)(key[i] - 'a'), z = (int)(cipherText[i] - 'a');
+                char y = (char)(((((z - x) % 26) + 26) % 26) + 'a');
+                plainText += y;
+            }
+            return plainText;
         }
 
         public string Encrypt(string plainText, string key)
         {
-            throw new NotImplementedException();
+            if (key.Length != plainText.Length)
+                key = GetKeyStream(key, plainText);
+            string cipherText = "";
+            for (int i = 0; i < key.Length; i++)
+            {
+                int x = (int)(key[i] - 'a'), y = (int)(plainText[i] - 'a');
+                char z = (char)(((x + y) % 26) + 'a');
+                cipherText += z;
+            }
+            return cipherText;
+        }
+        
+        public string GetKeyStream(string key, string plainText)
+        {
+            int idx = -1;
+            while (key.Length < plainText.Length)
+                key += key.ElementAt(((++idx) % key.Length));
+            return key;
+        }
+
+        public string RemoveRepeated(string key)
+        {
+            for (int i = 0; i < key.Length-1; i++)
+            {
+                string a = key.Substring(0, i+1);
+                string b = key.Substring(i + 1, key.Length - (i + 1));
+                if (a.IndexOf(b) == 0)
+                {
+                    key = RemoveRepeated(a);
+                    break;
+                }
+            }
+            return key;
         }
     }
 }
