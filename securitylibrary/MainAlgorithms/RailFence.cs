5a5085267eccc43522fc6019f026561d64d383f6
abdalla_maged <100102723+abdallamaged95@users.noreply.github.com>
2024-03-10 13:56:26 +0200

    RailFence

--- securitylibrary/MainAlgorithms/RailFence.cs
+++ securitylibrary/MainAlgorithms/RailFence.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,17 +11,65 @@ namespace SecurityLibrary
     {
         public int Analyse(string plainText, string cipherText)
         {
-            throw new NotImplementedException();
+            cipherText = cipherText.ToLower();
+            int key = 0;
+            
+            for (int j = 1; j < plainText.Length; j++)
+            {
+                if (plainText[j] == cipherText[1])
+                {
+                    key = j;
+                    string testPlain = Decrypt(cipherText, key);
+                    if (plainText.Equals(Preprocessing(testPlain)))
+                        break;
+                }
+            }
+            return key;
         }
 
         public string Decrypt(string cipherText, int key)
         {
-            throw new NotImplementedException();
+            char[] cText = cipherText.ToLower().ToCharArray();
+            int step = (int)Math.Ceiling((double)cipherText.Length / (double)key);
+            string plainText = "";
+            for (int i = 0; i < cText.Length; i++)
+                if (cText[i] != '-')
+                    for (int j = i; j <  cText.Length; j += step)
+                    {
+                        plainText += cText[j];
+                        cText[j] = '-';
+                    }
+            return plainText;
         }
 
         public string Encrypt(string plainText, int key)
         {
-            throw new NotImplementedException();
+            char[] pText = plainText.ToLower().ToCharArray();
+            string cipherText = "";
+            for (int i = 0; i < pText.Length; i++) 
+                if (pText[i] != '-')
+                {
+                    for (int j = i; j < pText.Length; j += key)
+                    {
+                        cipherText += pText[j];
+                        pText[j] = '-';
+                    }
+                }
+            return cipherText;
+        }
+
+        public string Preprocessing(string Text)
+        {
+            char[] tmpText = Text.ToLower().ToCharArray();
+            int idx = tmpText.Length - 1;
+            while (tmpText[idx] == 'x')
+            {
+                tmpText[idx] = '-';
+                idx--;
+            }
+            Text = new string(tmpText);
+            Text = Text.Replace("-", string.Empty);
+            return Text;
         }
     }
 }
