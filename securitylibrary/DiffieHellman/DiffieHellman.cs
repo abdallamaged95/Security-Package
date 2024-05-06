7e7ebe8421ecc4459a86556c8b820ff39c37a67a
abdalla_maged <100102723+abdallamaged95@users.noreply.github.com>
2024-05-07 02:05:22 +0300

    Diffie_Hellman

--- securitylibrary/DiffieHellman/DiffieHellman.cs
+++ securitylibrary/DiffieHellman/DiffieHellman.cs
@@ -10,7 +10,19 @@ namespace SecurityLibrary.DiffieHellman
     {
         public List<int> GetKeys(int q, int alpha, int xa, int xb)
         {
-            throw new NotImplementedException();
+            List<int> keys = new List<int>() { 0, 0 };
+            int ya = PowerModulo(alpha, xa, q);
+            int yb = PowerModulo(alpha, xb, q);
+            keys[0] = PowerModulo(yb, xa, q);
+            keys[1] = PowerModulo(ya, xb, q);
+            return keys;
+        }
+
+        public int PowerModulo(int b, int p, int m)
+        {
+            if (p == 1)
+                return b % m;
+            return (PowerModulo(b, p - 1, m) * b) % m;
         }
     }
 }
